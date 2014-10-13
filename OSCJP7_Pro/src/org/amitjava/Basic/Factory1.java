da1c59f350b9f71a00d6d5960c6bd6618e87ce87
JAmit761983 <amit761983@yahoo.co.uk>
2014-10-13 11:30:50 +0100

    Initial checking for OSCJP7 Tool.

---
+++ OSCJP7_Pro/src/org/amitjava/Basic/Factory1.java
@@ -0,0 +1,11 @@
+package org.amitjava.Basic;
+
+public class Factory1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to SCJP 7");
+	}
+
+}
